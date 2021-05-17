09a80d9d9015a7d2c1ab2f997a388128af8c5e88
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-17 15:50:16 +0530

    Update HelloControllerTest.java

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -13,12 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-
 
 
 @RunWith(SpringRunner.class)
